1a1daef7661dfd456bc8ecd9042dca56c846722a
pend <p.end@ya.ru>
2023-06-09 19:03:34 +0700

    First Activity Main

---
+++ app/src/main/java/ru/synergy/chooseyourtechnic/MainActivity.java
@@ -0,0 +1,21 @@
+package ru.synergy.chooseyourtechnic;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+/*    public void onClick(View v){
+        Toast.makeText(this, "я умею нажиматься", Toast.LENGTH_LONG).show();
+
+    }*/
+}
