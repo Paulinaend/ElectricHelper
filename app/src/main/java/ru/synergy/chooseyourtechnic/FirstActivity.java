2fe5403db45287e0d895f5987e1d5bb081cb5a78
pend <p.end@ya.ru>
2023-06-09 20:56:18 +0700

    Added SplashScreen

---
+++ app/src/main/java/ru/synergy/chooseyourtechnic/FirstActivity.java
@@ -0,0 +1,18 @@
+package ru.synergy.chooseyourtechnic;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
--- app/src/main/java/ru/synergy/chooseyourtechnic/MainActivity.java
+++ app/src/main/java/ru/synergy/chooseyourtechnic/MainActivity.java
@@ -2,20 +2,26 @@ package ru.synergy.chooseyourtechnic;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splashscreen);
+
+         new Handler().postDelayed(new Runnable(){
+                @Override
+                public void run() {
+                    Intent mainIntent = new Intent(MainActivity.this, FirstActivity.class);
+                    startActivity(mainIntent);
+                    finish();
+                }
+            }, SPLASH_DISPLAY_LENGTH);
+        }
     }
-
-/*    public void onClick(View v){
-        Toast.makeText(this, "я умею нажиматься", Toast.LENGTH_LONG).show();
-
-    }*/
-}
