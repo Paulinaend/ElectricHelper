a452d64fb3fb56ce7ef6a8f7f7021f346318aeee
pend <p.end@ya.ru>
2023-06-12 00:17:18 +0700

    Added conditions and equatoins (but values dont set into EditText)

--- app/src/main/java/ru/synergy/chooseyourtechnic/FirstActivity.java
+++ app/src/main/java/ru/synergy/chooseyourtechnic/FirstActivity.java
@@ -9,7 +9,10 @@ import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class FirstActivity extends AppCompatActivity {
-
+    float powerset=1;
+    float voltageset=1;
+    float phaseset=1;
+    float cosfiset=1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +27,7 @@ public class FirstActivity extends AppCompatActivity {
         switcher.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+                SetFields();
                 Intent i = new Intent(FirstActivity.this, ResultSwitcher.class);
                 startActivity(i);
             }
@@ -32,6 +36,7 @@ public class FirstActivity extends AppCompatActivity {
         cable.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+                SetFields();
                 Intent i = new Intent(FirstActivity.this, ResultCable.class);
                 startActivity(i);
             }
@@ -54,39 +59,50 @@ public class FirstActivity extends AppCompatActivity {
         });
 
     }
-    private void solution() {
+
+    public void SetFields() {
         EditText powerSet = (EditText) findViewById(R.id.PowerSet);
         EditText voltageSet = (EditText) findViewById(R.id.VoltageSet);
         EditText phaseSet = (EditText) findViewById(R.id.PhaseSet);
         EditText cosFiSet = (EditText) findViewById(R.id.CosFiSet);
-
-        float powerset = 1;
-        float voltageset = 1;
-        float phaseset = 1;
-        float cosfiset = 1;
 
         String powers = powerSet.getText().toString();
         String voltages = voltageSet.getText().toString();
         String phases = phaseSet.getText().toString();
         String cosfis = cosFiSet.getText().toString();
 
-        if(!powers.equals("") && powers!=null){
-            powerset = Integer.parseInt(powerSet.getText().toString());
+        if (!powers.equals("")) {
+            powerset = Float.parseFloat(powers);//powerSet.getText().toString()
         }
-        if(!voltages.equals("") && voltages!=null){
-            voltageset = Integer.parseInt(voltageSet.getText().toString());
+        if (!voltages.equals("")) {
+            voltageset = Float.parseFloat(voltages);//voltageSet.getText().toString()
         }
-        if(!phases.equals("") && phases!=null){
-            phaseset = Integer.parseInt(phaseSet.getText().toString());
+        if (!phases.equals("")) {
+            phaseset = Float.parseFloat(phases);//phaseSet.getText().toString()
         }
-        if(!cosfis.equals("") && cosfis!=null){
-            cosfiset = Integer.parseInt(cosFiSet.getText().toString());
+        if (!cosfis.equals("")) {
+            cosfiset = Float.parseFloat(cosfis);//cosFiSet.getText().toString()
         }
-
-        float solution = 0;
-
+    }
 
 
+    public float getPower()
+    {
+        return this.powerset;
+    }
+    public float getVoltage()
+    {
+        return this.voltageset;
+    }
+
+    public float getPhase()
+    {
+        return this.phaseset;
+    }
+
+    public float getCosFi()
+    {
+        return this.cosfiset;
     }
 
 }
--- app/src/main/java/ru/synergy/chooseyourtechnic/ResultSwitcher.java
+++ app/src/main/java/ru/synergy/chooseyourtechnic/ResultSwitcher.java
@@ -1,18 +1,46 @@
 package ru.synergy.chooseyourtechnic;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultSwitcher extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resultswitcher);
+
+        FirstActivity load = new FirstActivity();
+        float power = load.getPower();
+        float voltage = load.getVoltage();
+        float phase = load.getPhase();
+        float cosfi = load.getCosFi();
+
+
+        if(phase==3) {
+            float result = (float) (power / (voltage * cosfi * (Math.sqrt(3))));
+            String res = Float.toString(result);
+            TextView answer = (TextView) findViewById(R.id.currentswitcher);
+            answer.setText(res);
+        }
+        else if(phase==1)
+        {
+            float result = (float) power / voltage;
+            String res = Float.toString(result);
+            TextView answer = (TextView) findViewById(R.id.currentswitcher);
+            answer.setText(res);
+        }
+        else{
+            TextView answer = (TextView) findViewById(R.id.currentswitcher);
+            answer.setText("Введите корректное число фаз");
+        }
 
         final ImageButton backsw = (ImageButton) findViewById(R.id.backswitcher);
 
