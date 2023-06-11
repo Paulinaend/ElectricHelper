68eeb99da322f532c8816e7d370e3fe9ad1ebea0
pend <p.end@ya.ru>
2023-06-11 18:55:53 +0700

    Added backbuttons

--- app/src/main/java/ru/synergy/chooseyourtechnic/ResultCable.java
+++ app/src/main/java/ru/synergy/chooseyourtechnic/ResultCable.java
@@ -1,6 +1,10 @@
 package ru.synergy.chooseyourtechnic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ResultCable extends AppCompatActivity {
@@ -8,5 +12,15 @@ public class ResultCable extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resultcable);
+
+        final ImageButton backcab = (ImageButton) findViewById(R.id.backcable);
+
+        backcab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(ResultCable.this, FirstActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/ru/synergy/chooseyourtechnic/ResultSwitcher.java
+++ app/src/main/java/ru/synergy/chooseyourtechnic/ResultSwitcher.java
@@ -1,6 +1,10 @@
 package ru.synergy.chooseyourtechnic;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +13,15 @@ public class ResultSwitcher extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.resultswitcher);
+
+        final ImageButton backsw = (ImageButton) findViewById(R.id.backswitcher);
+
+        backsw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                Intent i = new Intent(ResultSwitcher.this, FirstActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
