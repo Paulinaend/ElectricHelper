49bc32df9a6da6848335702fd88165266baaf3c5
pend <p.end@ya.ru>
2023-06-17 16:19:46 +0700

    Get values in SavedData activity and save them in file (save/open)

--- app/src/main/java/ru/synergy/chooseyourtechnic/SavedData.java
+++ app/src/main/java/ru/synergy/chooseyourtechnic/SavedData.java
@@ -10,6 +10,8 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -17,6 +19,8 @@ import java.io.IOException;
 public class SavedData extends AppCompatActivity {
 
     private final static String FILE_NAME = "content.txt";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,43 +41,83 @@ public class SavedData extends AppCompatActivity {
         TextView answer = (TextView) findViewById(R.id.savedvalues);
         answer.setText("При нагрузке " + powerstr + " Вт, " + "напряжении " + voltagestr + " В, " + "количестве фаз " + phasestr + ", " + "коэффициенте нагрузки " + cosFistr + " - ток равен " + current + " А на фазу");
 
-        //сохранение файла
-       /* public void saveText(View view){
-            FileOutputStream fos = null;
-            try {
-                EditText textBox = (EditText) findViewById(R.id.editor);
-                String text = textBox.getText().toString();
-
-                fos = new FileOutputStream(getExternalFilePath());  //openFileOutput(FILE_NAME, MODE_PRIVATE);
-                fos.write(text.getBytes());
-                Toast.makeText(this, "Файл успешно сохранен", Toast.LENGTH_SHORT).show();
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-                Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-            } catch (IOException e) {
-                e.printStackTrace();
-                Toast.makeText(this,e.getMessage(), Toast.LENGTH_SHORT).show();
-            } finally {
-
-                try {
-                    if(fos != null){
-                        fos.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
-                }
-            }
-        }*/
 
         final ImageButton backsaved = (ImageButton) findViewById(R.id.backsaved);
-
         backsaved.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 Intent i = new Intent(SavedData.this, FirstActivity.class);
                 startActivity(i);
             }
         });
     }
+
+    private File getExternalFilePath() {
+        return new File(getExternalFilesDir(null), FILE_NAME);
+    }
+
+    //сохранение файла
+
+    public void saveText(View view) {
+        FileOutputStream fos = null;
+        try {
+            TextView textBox = (TextView) findViewById(R.id.savedvalues);
+            String text = textBox.getText().toString();
+
+            fos = new FileOutputStream(getExternalFilePath());  //openFileOutput(FILE_NAME, MODE_PRIVATE);
+            fos.write(text.getBytes());
+            Toast.makeText(this, "Данные успешно сохранены", Toast.LENGTH_LONG).show();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+
+            try {
+                if (fos != null) {
+                    fos.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    //открытие файла
+    public void openText(View view){
+        FileInputStream fin = null;
+        TextView textView = (TextView) findViewById(R.id.text);
+
+        File file = getExternalFilePath();
+
+        try{
+            fin = new FileInputStream(file); //openFileInput(FILE_NAME);
+            byte[] bytes = new byte[fin.available()];
+            fin.read(bytes);
+            String text = new String(bytes);
+            textView.setText(text);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+        } finally {
+
+            try{
+                if(fin != null){
+                    fin.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+
 }
